748b3922e3ca57a283d33c63a4dacc6e342ce03d
wushenglu <thomaswuhan@yahoo.com>
2021-11-06 21:41:58 -0400

    Integrate old change

--- app.js
+++ app.js
@@ -24,5 +24,5 @@ app.get('/profile', requiresAuth(), (req, res) =>{
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>{
-    console.log('listening on port ${port}');
+    console.log(`listening on port ${port}`);
 });
